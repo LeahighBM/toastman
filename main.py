ab309ea26a820c6c8f1f31a5446e8cd2d2cf5c01
LeahighBM <bleahighmc@gmail.com>
2025-07-25 15:50:37 -0600

    more things

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyperclip
 import requests
 from textual import on
 from textual.app import App, ComposeResult
-from textual.containers import HorizontalScroll
+from textual.containers import HorizontalScroll, Vertical
 from textual.containers import Container
 from textual.widgets import (
     Static, 
@@ -13,16 +13,25 @@ from textual.widgets import (
     Button, 
     Header, 
     TextArea,
+    TabbedContent,
+    TabPane,
+    Label
 )
 
 class Toastman(App):
     CSS_PATH = "css/toastman.tcss"
     BINDINGS = [
         ("q", "quit", "Quit"),
+        ("b", "toggle_sidebar", "Toggle Sidebar"),
     ]
 
     def compose(self) -> ComposeResult:
         yield Header()
+
+        yield Container(
+            Static("https://pokeapi.co/api/v2/pokemon", id="sidebar_content"),
+            id="sidebar"
+        )
 
         with HorizontalScroll():
             yield Select(
@@ -31,6 +40,7 @@ class Toastman(App):
                     ("POST", "POST"),
                     ("PUT", "PUT"),
                     ("DELETE", "DELETE"),
+                    ("OPTIONS", "OPTIONS")
                 ],
                 value="GET",
                 id="http_verb_select"
@@ -38,21 +48,26 @@ class Toastman(App):
             )
             yield Input(
                 placeholder="https://example.com/items",
-                id="url_bar")
+                id="url_bar",)
             
-            yield Button("Send", id="send_button")
+            yield Button("SEND", variant="primary", id="send_button")
 
+        with TabbedContent():
+            yield TabPane("Tab 1", Label("Tab 1"))
+            yield TabPane("POST Body", TextArea(id="post_body",language="json",))
+            yield TabPane("Tab 3", Label("content"))
     
         with Container():
             yield TextArea(
                 read_only=True,
                 id="response_text"
                 )
-            yield Button("Copy to clipboard", id="copy")
+            yield Button("Copy to clipboard", variant="warning", id="copy")
             
         yield Footer()
 
     @on(Button.Pressed, "#send_button")
+    @on(Input.Submitted, "#url_bar")
     def send_request(self, event) -> None:
         select_obj = self.query_one("#http_verb_select", Select)
         verb = select_obj.value
@@ -61,6 +76,9 @@ class Toastman(App):
         url = input_obj.value
         print(verb, url)
 
+        # the smart thing to do would be to check that the URL is not None or "" before we get here
+        # but I tried that and it broke everything... so we are stuck with checking for the 
+        # MissingSchema for every verb  
         match verb:
             case "GET":
                 try:
@@ -71,18 +89,41 @@ class Toastman(App):
                 except requests.exceptions.MissingSchema as e:
                     message = f"Missing URL. {e}"
                     self.update_text(message)
+                except requests.HTTPError as he:
+                    self.update_text(f"An error occurred during or as a result of the last request: {he}")
+            case "POST":
+                try:
+                    post_body_obj = self.query_one("#post_body")
+                    body = post_body_obj.text
+                    body = json.dumps(body)
+                    resp = requests.post(url=url, data=body)
+                except requests.exceptions.MissingSchema as e:
+                    message = f"Missing URL. {e}"
+                    self.update_text(message)
+            case "OPTIONS":
+                try:
+                    resp = requests.options(url=url)
+                    resp.raise_for_status()
+                    if resp.headers.get( 'Access-Control-Allow-Methods') is not None:
+                        self.update_text(resp.headers.get( 'Access-Control-Allow-Methods'))
+                    else:
+                        self.update_text(resp.headers)
+                except requests.exceptions.MissingSchema as e:
+                    message = f"Missing URL. {e}"
+                    self.update_text(message)
+
+    def on_mount(self) -> None:  
+        self.theme = "dracula"
 
     @on(Button.Pressed, "#copy")
     def copy_button_press(self, event) -> None:
         self.copy_text()
 
-
     def update_text(self, text) -> None: 
         response_text_obj = self.query_one("#response_text", TextArea)
         response_text_obj.text = text   
 
-
-    def copy_text(self):
+    def copy_text(self) -> None:
         text_obj = self.query_one("#response_text")
         text = text_obj.text 
 
@@ -92,6 +133,9 @@ class Toastman(App):
         except pyperclip.PyperclipException as e:
             self.notify(f"Could not copy to clipboard. {e}")
 
+    def action_toggle_sidebar(self) -> None:
+        sidebar = self.query_one("#sidebar")
+        sidebar.toggle_class("visible")
 
 if __name__ == "__main__":
     Toastman().run()
